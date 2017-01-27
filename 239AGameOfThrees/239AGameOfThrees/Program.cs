89c3b2b2715b89e755d564cf08b69e848a82bcb7
Joost van Uitert <joostvanuitert@gmail.com>
2017-01-27 13:27:41 +0100

    First working version

--- 239AGameOfThrees/239AGameOfThrees/Program.cs
+++ 239AGameOfThrees/239AGameOfThrees/Program.cs
@@ -10,7 +10,41 @@ namespace _239AGameOfThrees
     {
         static void Main(string[] args)
         {
+            int Number;
+            int.TryParse(Console.ReadLine(), out Number);
 
+            // While the number is not 1
+            while (Number != 1)
+            {
+                // If the number is divisable by 3
+                if(Number % 3 != 0)
+                {
+                    // Is not divisable by 3, should check if adding or subtracting one is better
+
+                    // If subtracting one makes it divisable by 3
+                    if ((Number - 1) % 3 == 0)
+                    {
+                        Console.WriteLine(Number + " - 1");
+                        Number = (Number - 1) / 3;
+                    }
+                    // If adding one makes it divisable by 3
+                    else if ((Number + 1) % 3 == 0)
+                    {
+                        Console.WriteLine(Number + " + 1");
+                        Number = (Number + 1) / 3;
+                    }
+                } else
+                {
+                    // Then devide it by 3
+                    Console.WriteLine(Number);
+                    Number = Number / 3;
+                }
+            }
+
+            // Puzzle is solved: 1 is left after deviding by three
+            Console.WriteLine(Number);
+
+            Console.ReadLine();
         }
     }
 }
