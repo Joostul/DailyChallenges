7cf57e69cb1be7f3d19175bae6b1c02e7157fa0e
Joost van Uitert <joostvanuitert@gmail.com>
2017-01-27 16:24:22 +0100

    Working on game logic.

--- 238FalloutHackingGame/238FalloutHackingGame/Program.cs
+++ 238FalloutHackingGame/238FalloutHackingGame/Program.cs
@@ -10,6 +10,42 @@ namespace _238FalloutHackingGame
     {
         static void Main(string[] args)
         {
+            int difficulty;
+            int guessesLeft = 4;
+            string guess;
+                
+            int.TryParse(Console.ReadLine(), out difficulty);
+
+            foreach(string word in _words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("Now guess a word ({0} guesses left): ", guessesLeft);
+            guess = Console.ReadLine().ToUpper();
+
+            while(guessesLeft < 5)
+            {
+                if (_words.Contains(guess))
+                {
+                    if (guess == _words[0])
+                    {
+                        Console.WriteLine("You guessed correctly!");
+                    }
+                    else
+                    {
+                        guessesLeft -= 1;
+                        Console.WriteLine("You guessed incorrectly, please guess again.");
+                        // Check how many letters are correct.
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your word is not in the list.");
+                }
+            }
+            Console.ReadKey();
         }
+
+        static List<string> _words = new List<string>(new string[] { "TESTING", "INTERST", "UPLOADS", "CLOTHES", "WORRIED" });
     }
 }
